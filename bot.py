6fa56400f9d46fa97fa8cd842d351679a448ee85
Mikhail Chaychuk <45295266+EmperorMikhail223@users.noreply.github.com>
2019-03-26 20:47:56 +0300

    Add files via upload
    Добавлен функционал Истории

--- bot.py
+++ bot.py
@@ -3,11 +3,15 @@ import telebot
 from telebot import types
 from random import choice
 from datetime import datetime
-from bot_functions import farewell, services, history, feedback, navigation, privileges, bot_help
-# from Bot_history import history
-
+from bot_functions import services, feedback, navigation, privileges, bot_help
 
 bot = telebot.TeleBot(config.token)
+
+
+def farewell(message):
+    farewell_messages = ['До свидания!', 'До встречи!']
+    goodbye = choice(farewell_messages)
+    bot.send_message(message.chat.id, goodbye)
 
 
 @bot.message_handler(commands=["start"])
@@ -25,14 +29,15 @@ def start_message(message):
     bot.send_message(message.chat.id, greeting)
     bot.send_message(message.chat.id, config.description)
     keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data=name) for name in ['Да', 'Нет']])
+    keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data='start_{}'.format(name)) for name in ['Да',
+                                                                                                             'Нет']])
     bot.send_message(message.chat.id, "Я могу Вам помочь?", reply_markup=keyboard)
 
 
-@bot.callback_query_handler(func=lambda button: True)
+@bot.callback_query_handler(func=lambda button: 'start' in button.data and True)
 def keyboard_answer(button):
     msg = button.message
-    if button.data == 'Да':
+    if button.data.split('_')[1] == 'Да':
         main_menu(msg)
     else:
         farewell(msg)
@@ -49,7 +54,7 @@ def main_menu(message):
 
 def menu_functions(m):
     if m.text == 'История':
-        history(m, 'Хотите узнать об истории компании?')
+        short_history(m)
     elif m.text == 'Сервисы':
         services(m.chat.id)
     elif m.text == 'Привилегии':
@@ -62,10 +67,53 @@ def menu_functions(m):
         bot_help(m.chat.id)
 
 
-'''@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
-'''
+def last_message(message):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data='last_{}'.format(name)) for name in ['Да',
+                                                                                                            'Нет']])
+    bot.send_message(message.chat.id, "Вам еще нужна помощь?", reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda button: 'last' in button.data and True)
+def last_message_answer(button):
+    if button.data.split('_')[1] == 'Да':
+        main_menu(button.message)
+    else:
+        farewell(button.message)
+
+
+@bot.message_handler(commands=["history", "история"])
+def short_history(message):
+    bot.send_message(message.chat.id, '*short history*')
+    start_history(message)
+
+
+def start_history(message):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(*[types.InlineKeyboardButton(text=name, callback_data='history1_{}'.format(name)) for name in ['Да',
+                                                                                                                'Нет']])
+    bot.send_message(message.chat.id, 'Хотите узнать больше?', reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda button: 'history1' in button.data and True)
+def start_history_answer(button):
+    if button.data.split('_')[1] == 'Да':
+        short_history_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        short_history_keyboard.add(*[types.KeyboardButton(name) for name in ['History№1', 'History№2', 'History№3',
+                                                                             'Меню']])
+        msg = bot.send_message(button.message.chat.id, "О чём хотите узнать?", reply_markup=short_history_keyboard)
+        bot.register_next_step_handler(msg, history)
+    else:
+        last_message(button.message)
+
+
+def history(m):
+    if m.text == 'Меню':
+        main_menu(m)
+    else:
+        bot.send_message(m.chat.id, config.histories[m.text])
+        start_history(m)
+
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
